36ab3b1f4404f583c5b21df1a0161dd32b00b97c
Shivraj Rath <shivrajrath@gmail.com>
2017-05-25 21:36:24 -0500

    Initial project structure commit

---
+++ app.ts
@@ -0,0 +1,3 @@
+import {SPELLINGS} from 'spellings';
+
+
---
+++ dist/tsc.js
@@ -0,0 +1,37 @@
+define("spellings", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.SPELLINGS = {
+        A: 'Alpha',
+        B: 'Bravo',
+        C: 'Charlie',
+        D: 'Delta',
+        E: 'Echo',
+        F: 'Foxtrot',
+        G: 'Golf',
+        H: 'Hotel',
+        I: 'India',
+        J: 'Juliett',
+        K: 'Kilo',
+        L: 'Lima',
+        M: 'Mike',
+        N: 'November',
+        O: 'Oscar',
+        P: 'Papa',
+        Q: 'Quebec',
+        R: 'Romeo',
+        S: 'Sierra',
+        T: 'Tango',
+        U: 'Uniform',
+        V: 'Victor',
+        W: 'Whiskey',
+        X: 'X-ray',
+        Y: 'Yankee',
+        Z: 'Zulu'
+    };
+});
+define("app", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+});
+//# sourceMappingURL=tsc.js.map
